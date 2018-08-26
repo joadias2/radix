6144dea11d57027fe61e250bee2c842c7291d808
hastyy <jpm.dias@campus.fct.unl.pt>
2018-08-26 12:45:05 +0100

    Code splitting: lazy loading.

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import Counter from 'components/Counter';
 
 import styles from './App.scss';
 import logo from 'images/logo.svg';
+import Lazy from 'components/Lazy';
 
 const initialState = {
-  counter: 0
+  counter: 0,
+  Lazy: null
 };
 
 type State = typeof initialState;
@@ -20,6 +22,11 @@ export default class App extends Component<object, State> {
         counter: prevState.counter + 1
       }));
     }, 1000);
+
+    setTimeout(async () => {
+      const module = await import('./components/Lazy');
+      this.setState({ Lazy: module.default });
+    }, 1500);
   }
 
   public componentWillUnmount() {
@@ -27,9 +34,12 @@ export default class App extends Component<object, State> {
   }
 
   public render() {
+    const { Lazy } = this.state;
+
     return (
       <div className={styles.container}>
         <h1>Hello World!</h1>
+        {Lazy ? <Lazy /> : null}
         <img src={logo} className="App-logo" alt="logo" />
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta eos labore adipisci?</p>
         <Counter counter={this.state.counter} />
---
+++ src/components/Lazy.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Lazy() {
+  return (
+    <h3>
+      I was loaded in a <span>lazy</span> way!
+    </h3>
+  );
+}
